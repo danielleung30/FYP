b1e8317b4673ff5ec6c884644ef6c80b8ca243f6
Leung Yu Hong <daniel@Leungde-MacBook-Pro.local>
2018-12-28 16:57:49 +0800

    text always face player now

---
+++ Assets/checkDis.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class checkDis : MonoBehaviour
+{
+    public int distance;
+    public GameObject player;
+    Component meshRender;
+  
+    Vector3 pos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<MeshRenderer>().enabled = false;
+       
+        pos = transform.position;
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(player.transform.position, pos)<distance) {
+
+            GetComponent<MeshRenderer>().enabled = true;
+            transform.LookAt(player.transform);
+
+        }
+        else {
+
+            GetComponent<MeshRenderer>().enabled = false;
+           
+        }
+    }
+}

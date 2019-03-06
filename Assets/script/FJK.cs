0270bd9dbe4a1404325549e098e6cbc9605101b5
Cheonnnnnnng <37610773+Cheonnnnnnng@users.noreply.github.com>
2019-03-06 13:48:45 +0800

    fog

---
+++ Assets/script/FJK.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FJK : MonoBehaviour
+{
+    public GameObject Obj;
+    public float fogtemp = 0.0f;
+    public Slider slider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        MeshRenderer m = Obj.GetComponent<MeshRenderer>();
+        m.material.SetColor("_TintColor", new Color(1, 1, 1, 0));
+
+    }
+
+
+    public void Update()
+    {
+        MeshRenderer m = Obj.GetComponent<MeshRenderer>();
+        m.material.SetColor("_TintColor", new Color(1, 1, 1, slider.value)); 
+    }
+}

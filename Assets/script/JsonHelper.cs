bdf1dbccf9aa2eae95a56b4e1bffe7219f1cd0ba
ethanlam <hellowah5@gmail.com>
2019-03-09 19:56:35 +0800

    spawn

---
+++ Assets/script/JsonHelper.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JsonHelper 
+{
+    public static T[] getJsonArray<T>(string json)
+    {
+        string newJson = "{ \"array\": " + json + "}";
+        Wrapper<T> wrapper = JsonUtility.FromJson<Wrapper<T>>(newJson);
+        return wrapper.array;
+    }
+
+    [System.Serializable]
+    private class Wrapper<T>
+    {
+        public T[] array;
+    }
+}
+
+[System.Serializable]
+public class RootObject
+{
+    public string _id;
+    public string TITLE;
+    public string LATITUDE;
+    public string LONGITUDE;
+    public string DESCRIPTION;
+    public string VIDEO_ID;
+}
+
---
+++ Assets/script/testCheckDis.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class testCheckDis : MonoBehaviour
+{
+    public int distance;
+    public GameObject player;
+    public RectTransform _rectTransform;
+
+    Vector3 pos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rectTransform = GetComponent<RectTransform>();
+        pos = _rectTransform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Debug.Log(pos);
+        // Debug.Log(player.transform.position);
+        // Debug.Log(Vector3.Distance(player.transform.position, pos));
+        if (Vector3.Distance(player.transform.position, pos) < distance)
+        {
+
+            //transform.LookAt(player.transform);
+            GetComponentInChildren<MeshRenderer>().enabled = true;
+            _rectTransform.rotation = Quaternion.LookRotation(_rectTransform.position - player.transform.position);
+        }
+        else
+        {
+
+            //GetComponent<MeshRenderer>().enabled = false;
+            GetComponentInChildren<MeshRenderer>().enabled = false;
+
+        }
+    }
+}
